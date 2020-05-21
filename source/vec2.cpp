21748c00286e30585bf8125132fd5399ffa49d99
Jonas Roquette <roquettejonas@gmail.com>
2020-05-21 23:13:00 +0200

    added free funcs

--- source/vec2.cpp
+++ source/vec2.cpp
@@ -1,5 +1,6 @@
 #include "vec2.hpp"
 
+//Member Funktionen
 Vec2& Vec2::operator+=(Vec2 const& v) {
     x += v.x;
     y += v.y;
@@ -26,4 +27,45 @@ Vec2& Vec2::operator/=(float s) {
     y /= s;
 
     return *this;
+}
+
+//Freie Funktionen
+Vec2 operator+(Vec2 const & u, Vec2 const & v) {
+    Vec2 newVec;
+    newVec.x = u.x + v.x;
+    newVec.y = u.y + u.y;
+
+    return newVec;
+}
+
+Vec2 operator-(Vec2 const & u, Vec2 const & v) {
+    Vec2 newVec;
+    newVec.x = u.x - v.x;
+    newVec.y = u.y - u.y;
+
+    return newVec;
+}
+
+Vec2 operator*(Vec2 const & v, float s) {
+    Vec2 newVec;
+    newVec.x = v.x * s;
+    newVec.y = v.y * s;
+
+    return newVec;
+}
+
+Vec2 operator/(Vec2 const & v, float s) {
+    Vec2 newVec;
+    newVec.x = v.x / s;
+    newVec.y = v.y / s;
+
+    return newVec;
+}
+
+Vec2 operator*(float s, Vec2 const & v) {
+    Vec2 newVec;
+    newVec.x = v.x * s;
+    newVec.y = v.y * s;
+
+    return newVec;
 }
--- source/vec2.hpp
+++ source/vec2.hpp
@@ -6,12 +6,18 @@ struct Vec2 {
     float x{0.0f};
     float y{0.0f};
 
+    //Member Funktionen
     Vec2& operator+=(Vec2 const& v);
     Vec2& operator-=(Vec2 const& v);
     Vec2& operator*=(float s);
     Vec2& operator/=(float s);
 };
 
+Vec2 operator+(Vec2 const & u, Vec2 const & v);
+Vec2 operator-(Vec2 const & u, Vec2 const & v);
+Vec2 operator*(Vec2 const & v, float s);
+Vec2 operator/(Vec2 const & v, float s);
+Vec2 operator*(float s, Vec2 const & v);
 
 
-#endif
+#endif //VEC2_HPP
